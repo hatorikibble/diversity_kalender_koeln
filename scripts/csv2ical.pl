470544371bdf5cf6d330d7da87246398d927b880
Peter Mayr <mayr@hbz-nrw.de>
2018-01-31 16:17:33 +0100

    Daten fuer 2018 hinzugefügt, Skript angepasst und neue ICS Datei erstellt

--- scripts/csv2ical.pl
+++ scripts/csv2ical.pl
@@ -10,6 +10,9 @@ csv2ical.pl - converts csv File to ICal data
 
 =cut
 
+use strict;
+use warnings;
+
 use Data::ICal;
 use Data::ICal::Entry::Event;
 use DateTime;
@@ -17,49 +20,66 @@ use DateTime::Event::ICal;
 use DateTime::Format::ICal;
 use DateTime::Format::Strptime;
 use Text::xSV;
+use Try::Tiny;
 
-my $file     = "../Diversity_Kalender_2017.csv";
-my $csv      = Text::xSV->new( sep => ";" );
-my $calendar = Data::ICal->new(
-    calname    => "Kölner Diversity Kalender 2017",
-    auto_uid   => "true",
-    rfc_strict => "true"
+my $file = "../Diversity_Kalender_2018.csv";
+my $csv = Text::xSV->new( sep => ";" );
+my $calendar = Data::ICal->new( calname    => "Kölner Diversity Kalender 2017",
+                                auto_uid   => "true",
+                                rfc_strict => "true"
 );
-my $strp = DateTime::Format::Strptime->new(
-    pattern  => '%d.%m.%Y',
-    on_error => 'croak',
-);
+my $strp =
+    DateTime::Format::Strptime->new( pattern => '%d.%m.%Y',
+                                     on_error => 'croak', );
 my $event   = undef;
 my $buffer  = undef;
 my $summary = undef;
+my $dt_von  = undef;
+my $dt_bis  = undef;
 
 $csv->open_file($file);
 $csv->read_header();
 
 while ( $csv->get_row() ) {
-    my ( $datum, $feiertag, $religion )
-        = $csv->extract(qw(Datum Feiertag Religion));
+    my ( $von, $bis, $feiertage, $art, $beschreibung )
+        = $csv->extract(qw(von bis Feiertage Art Beschreibung));
 
-    my $dt = $strp->parse_datetime($datum);
+    try {
+        $dt_von = $strp->parse_datetime($von);
+    }
+      catch { warn "Problem beim Parsen von $von: $_";
+	      
+	    };
+    if ( defined($bis) ) {
+        try {
+            $dt_bis = $strp->parse_datetime($bis);
+        }
+	  catch { warn "Problem beim Parsen von $bis: $_";
+		  
+		};
+    }
+    else {
+        $dt_bis = $dt_von->add( days => 1 );
+    }
 
     #print $dt->strftime('%Y%m%d');
-
-    $event = Data::ICal::Entry::Event->new();
+     $event = Data::ICal::Entry::Event->new();
     $event->add_properties(
-        summary     => $feiertag,
-        description => $religion,
-        dtstart     => "DATE:" . $dt->strftime('%Y%m%d'),
-        dtend       => "DATE:" . $dt->add( days => 1 )->strftime('%Y%m%d'),
-        dtstamp => DateTime::Format::ICal->format_datetime( DateTime->now ),
+            summary     => $feiertage,
+            description => sprintf( "(%s) %s", $art, $beschreibung || '' ),
+            dtstart     => "DATE:" . $dt_von->strftime('%Y%m%d'),
+            dtend       => "DATE:" . $dt_bis->strftime('%Y%m%d'),
+            dtstamp => DateTime::Format::ICal->format_datetime( DateTime->now ),
     );
 
     $calendar->add_entry($event);
 
-}
+} ## end while ( $csv->get_row() )
 
 $buffer = $calendar->as_string;
 
 # fix for all-day events:
 $buffer =~ s/DT(START|END):/DT$1;VALUE=/g;
-
+# fix for blank lines
+$buffer =~ s/\r\n\s\r\n/\r\n/g;
 print $buffer;
